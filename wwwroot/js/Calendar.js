49c54b50b91b546ccfb8ccbf0a46e63ddb5e858f
6187144 <6187144@cegep-heritage.qc.ca>
2022-05-20 12:35:21 -0400

    dateformatissues

--- wwwroot/js/Calendar.js
+++ wwwroot/js/Calendar.js
@@ -1,20 +1,17 @@
 
-    function twoDigiter(n){return n<10? '0'+n:''+n;}
-    const convert1224 = (t12) => {
-        const [time, modifier] = t12.split(' ');
-      
-        let [hours, minutes, secs] = time.split(':');
-      
-        if (hours === '12') {
-          hours = '00';
-        }
-      
-        if (modifier === 'PM') {
-          hours = parseInt(hours, 10) + 12;
-        }
-      
-        return `${twoDigiter(hours)}:${twoDigiter(minutes)}`;
-    }
+    // function twoDigiter(n){
+    //     if(n[0] ==0) {return n} else {return n<10? '0'+n:''+n;}}
+    // const convert1224 = (t12) => {
+    //     const [time, modifier] = t12.split(' ');
+    //     let [hours, minutes, secs] = time.split(':');
+    //     if (hours === '12') {
+    //       hours = '00';
+    //     }
+    //     if (modifier === 'PM') {
+    //       hours = parseInt(hours, 10) + 12;
+    //     }
+    //     return `${twoDigiter(hours)}:${twoDigiter(minutes)}`;
+    // }
 
     let eventsArr = [];
         let todoTable = document.getElementById("todoTable");
@@ -24,33 +21,36 @@
         for (let tr of trElem) {
             //console.log(tr);
             let tdElems = tr.getElementsByTagName("td");
-            let splitted =tdElems[2].innerText.split(" ");
-            let sdate = splitted[0].split("/");
-            let year;
-            let day;
-            let month;
+            //console.log(tdElems[2].innerText);
+            let sTime=moment(tdElems[2].innerText).format('YYYY-MM-DD hh:mm');
+            // let splitted =tdElems[2].innerText.split(" ");
+            // let sdate = splitted[0].split("/");
             // console.log(splitted[0]);
             // console.log(sdate[1]);
-            if (sdate[1]=="undefined") {
-                sdate = splitted[0].split("-");
-                day = twoDigiter(sdate[2]);
-                month = twoDigiter(sdate[1]);
-                year = sdate[0];
-            } else {
-            day = twoDigiter(sdate[1]);
-            month = twoDigiter(sdate[0]);
-            year = sdate[2];
-            }
-            let hrs =convert1224(splitted[1]+" "+splitted[2]);
+            
+            // if (sdate[1]=="undefined") {
+            //     sdate = splitted[0].split("-");
+            //     console.log(sdate[1]);
+            //     day = twoDigiter(sdate[2]);
+            //     month = twoDigiter(sdate[1]);
+            //     year = sdate[0];
+            // } else {
+            // let day = twoDigiter(sdate[1]);
+            // let month = twoDigiter(sdate[0]);
+            // let year = sdate[2];
+            //console.log(year[0]);
+            //}
+            //let hrs =convert1224(splitted[1]+" "+splitted[2]);
             //console.log(splitted[1]);
             let eventObj = {
                 id: tdElems[0].innerText,
                 title: tdElems[1].innerText,
-                start: year+"-"+month+"-"+day+" "+hrs,
+                //start: year+"-"+month+"-"+day+" "+hrs,
+                start:sTime,
             }
             eventsArr.push(eventObj);
         }
-        console.log(eventsArr);
+        //console.log(eventsArr);
         let calendarEl = document.getElementById('calendar');
         let calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
@@ -66,7 +66,6 @@
             window.location = "./Hike/Details?id=" + calEvent.event._def.publicId;
     
         },
-        
         // events: [
         //     {
         //         title:"dsfsdfsd",
