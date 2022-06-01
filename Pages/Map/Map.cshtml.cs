50b8360d079cf6c7d0233f59b2182794c8f2f3e1
LPP <2175801@cegep-heritage.qc.ca>
2022-05-31 20:43:47 -0400

    validation for route post

--- Pages/Map/Map.cshtml.cs
+++ Pages/Map/Map.cshtml.cs
@@ -2,26 +2,33 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Lab3.Models;
+using Lab3.Areas.Identity.Pages.Account.Manage;
 
 namespace Lab3.Pages_Map
 {
     public class MapModel : PageModel
     {
         private readonly StoreDBContext _context;
+        private readonly UserManager<HikeUser> _userManager;
+        private readonly ILogger<IndexModel> _logger;
 
         [BindProperty(SupportsGet = true)]
         public RouteDifficultyLevel? DifficultyLevel { get; set; }
         [BindProperty(SupportsGet = true)]
         public RouteLengthLevel? LengthLevel { get; set; }
 
-        public MapModel(StoreDBContext context)
+        public MapModel(StoreDBContext context,ILogger<IndexModel> logger, UserManager<HikeUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
+            _logger = logger;
         }
 
         // public IList<Map> Map { get;set; }
